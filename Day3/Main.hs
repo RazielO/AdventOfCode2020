3cdabe456e02403ee1f253e6264a857e4f9897d6
RazielO <oraziel_a@hotmail.com>
2020-12-05 10:52:27 -0600

    Day 3 part two solution

--- Day3/Main.hs
+++ Day3/Main.hs
@@ -2,21 +2,26 @@ module Main where
 
 import Data.List.Split (splitOn)
 
-isATree :: String -> Int -> Bool
-isATree line n = line !! pos == '#'
+isATree :: String -> Int -> Int -> Bool
+isATree line n right = line !! pos == '#'
   where
-    pos = (n * 3) `mod` length line
+    pos = (n * right) `mod` length line
 
-solve :: [String] -> Int
-solve lines =
+solve :: [String] -> Int -> Int -> Int
+solve lines right down =
   length $
     filter (== True) $
-      zipWith isATree lines [0 ..]
+      map (\x -> uncurry isATree x right) $
+        filter (\x -> snd x `mod` down == 0) (zip lines [0 ..])
 
 main :: IO ()
 main = do
   input <- readFile "input.txt"
   let lines = splitOn "\n" input
 
-  print "Part One Solution:"
-  print $ solve lines
+  putStrLn "Part One Solution:"
+  print $ solve lines 3 1
+
+  putStrLn "Part Two Solution:"
+  let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+  print $ product $ map (uncurry (solve lines)) slopes
