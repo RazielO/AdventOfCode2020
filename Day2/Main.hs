a955e0a32cd4be37b3395b9cf2d3e172c12ced59
RazielO <oraziel_a@hotmail.com>
2020-12-04 21:43:36 -0600

    Day 2 part two solution

--- Day2/Main.hs
+++ Day2/Main.hs
@@ -25,9 +25,17 @@ count entry = length $ filter (\x -> x == letter entry) (password entry)
 isValid :: Entry -> Bool
 isValid entry = lower entry <= count entry && count entry <= upper entry
 
+isValid2 :: Entry -> Bool
+isValid2 entry = first /= second
+  where
+    first = password entry !! (lower entry - 1) == letter entry
+    second = password entry !! (upper entry - 1) == letter entry
+
 main :: IO ()
 main = do
   input <- readFile "input.txt"
   let entries = map parseLine $ splitOn "\n" input
   putStrLn "Result of Part One:"
   print $ length $ filter isValid entries
+  putStrLn "Result of Part Two:"
+  print $ length $ filter isValid2 entries
