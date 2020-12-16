e10c206a6a664c47ebd4cc3d4c43cdd15cdc0203
RazielO <oraziel_a@hotmail.com>
2020-12-16 13:24:33 -0600

    Day 9 part two solution

--- Day9/Main.hs
+++ Day9/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.List.Split (splitOn)
+import Debug.Trace (trace)
 
 findSum :: [Int] -> Int -> (Bool, [Int])
 findSum [] _ = (False, [])
@@ -17,9 +18,28 @@ solveOne numbers current
   where
     try = findSum (take 25 (drop current numbers)) (numbers !! (current + 25))
 
+solveTwo' :: [Int] -> Int -> Int -> Int -> (Bool, [Int])
+solveTwo' numbers start size target
+  | sum nums == target = (True, nums)
+  | sum nums > target = (False, [])
+  | otherwise = solveTwo' numbers start (size + 1) target
+  where
+    nums = take size (drop start numbers)
+
+solveTwo :: [Int] -> Int -> Int -> Int
+solveTwo [] _ _ = -1
+solveTwo numbers target start
+  | fst try = minimum (snd try) + maximum (snd try)
+  | otherwise = solveTwo numbers target (start + 1)
+    where
+      try = solveTwo' numbers start 0 target
+
 main :: IO ()
 main = do
   input <- readFile "input.txt"
   let numbers = map (\x -> read x :: Int) (splitOn "\n" input)
+  let first = solveOne numbers 0
   putStrLn "Part One Solution:"
-  print $ solveOne numbers 0
+  print first
+  putStrLn "Part Two Solution:"
+  print $ solveTwo (filter (/= first) numbers) first 0
